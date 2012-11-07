8c504e4cb03e91989bbce32ba9d638369f3b6f9e
Vladimir Varankin <nek.narqo+git@gmail.com>
2012-11-08 02:36:46 +0400

    Levels renamed to the .[tech] scheme

---
+++ desktop.bundles/.bem/level.js
@@ -0,0 +1,15 @@
+var BEM = require('bem');
+
+exports.baseLevelPath = require.resolve('../../.bem/levels/bundles.js');
+
+exports.getConfig = function() {
+
+    return BEM.util.extend(this.__base() || {}, {
+        bundleBuildLevels: this.resolvePaths([
+            '../../bem-bl/blocks-common',
+            '../../bem-bl/blocks-desktop',
+            '../../desktop.blocks'
+        ])
+    });
+
+};

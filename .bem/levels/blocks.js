d8831051fa8404e644b4719f6a631d22766157c0
Vladimir Varankin <nek.narqo+git@gmail.com>
2012-11-30 19:07:56 +0400

    Refactiring for BEM levels config

---
+++ .bem/levels/blocks.js
@@ -0,0 +1,15 @@
+exports.getTechs = function() {
+
+    return {
+        'js': 'js',
+        'ie.css': 'ie.css',
+        'ie6.css': 'ie6.css',
+        'ie7.css': 'ie7.css',
+        'ie8.css': 'ie8.css',
+        'ie9.css': 'ie9.css',
+
+        'title.txt': 'title.txt',
+        'desc.wiki': ''
+    };
+
+};

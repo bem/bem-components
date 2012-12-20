723c21ea48cd3ac00787d7fa7db054c2b66f419b
Elena Musatova <span4ik@yandex-team.ru>
2012-12-20 19:52:01 +0200

    LEGO-8355 - [bl-controls] attach: Перенести блок во внешнюю библиотеку. Переместила css, ie.css, js, desk.wiki, title.txt блока attach.

---
+++ desktop.blocks/attach/attach.js
@@ -0,0 +1,182 @@
+/**
+ * @namespace
+ * @name Attach
+ */
+BEM.DOM.decl('attach', /** @lends Attach.prototype */ {
+
+    onSetMod : {
+
+        'js' : function() {
+
+            this._noFileText = this.elem('text').text();
+            this._update();
+
+            this.bindTo('reset', 'click', this.resetFile);
+
+        }
+
+    },
+
+    /**
+     * Возвращает значение контрола, путь к выбранному файлу
+     * @returns {String}
+     */
+    val : function() {
+
+        return this.findElem('control').val();
+
+    },
+
+    /**
+     * Сбросить выбранное значение контрола
+     * @returns {BEM.DOM}
+     */
+    resetFile : function() {
+
+        var buttonInput = this.findElem('control');
+
+        buttonInput.replaceWith(BEM.HTML.build({
+            block: 'attach',
+            elem: 'control',
+            tag: 'input',
+            attrs: { name: buttonInput.attr('name') || 'attachment', type: 'file' }
+        }));
+
+        this._update();
+        this.delMod(this.elem('reset'), 'visibility');
+
+        return this.trigger('reset');
+    },
+
+    /**
+     * @private
+     */
+    _update : function() {
+
+        var fileName = this._getFileByPath(this.val());
+
+        this._setFile(fileName);
+        this._setExtension(this._getExtension(fileName));
+
+        this.setMod(this.elem('reset'), 'visibility', 'visible');
+
+        fileName && this.trigger('change');
+    },
+
+    /**
+     * @private
+     * @param {String} path
+     * @returns {String}
+     */
+    _getFileByPath : function(path) {
+
+        return path.split('\\').pop();
+
+    },
+
+    /**
+     * @private
+     * @param {String} fileName
+     */
+    _setFile : function(fileName) {
+
+        this.setMod(this.elem('holder'), 'state', fileName ? '' : 'hidden');
+        this.elem('text').html(fileName || this._noFileText);
+    },
+
+    _extensionsToMods : {
+        'zip' : 'archive',
+        'rar' : 'archive',
+        'tar' : 'archive',
+        'gz' : 'archive',
+        '7z' : 'archive',
+        'gif' : 'gif',
+        'jpg' : 'jpg',
+        'jpeg' : 'jpg',
+        'png' : 'png',
+        'eml' : 'eml',
+        'exe' : 'exe',
+        'm4a' : 'audio',
+        'ogg' : 'audio',
+        'mp3' : 'mp3',
+        'wav' : 'wav',
+        'wma' : 'wma',
+        'flv' : 'video',
+        'mov' : 'mov',
+        'wmv' : 'wmv',
+        'mp4' : 'mp4',
+        'avi' : 'avi',
+        'xls' : 'xls',
+        'doc' : 'doc',
+        'docx' : 'doc',
+        'txt' : 'txt',
+        'pdf' : 'pdf',
+        'ppt' : 'ppt'
+    },
+
+    /**
+     * @private
+     * @param {String} fileName
+     * @returns {String}
+     */
+    _getExtension : function(fileName) {
+
+        return this._extensionsToMods[fileName.split('.').pop().toLowerCase()] || '';
+
+    },
+
+    /**
+     * @private
+     * @param {String} extension
+     */
+    _setExtension : function(extension) {
+
+        this.setMod(this.elem('holder'), 'file', extension || this.params.unknownType);
+
+    },
+
+    getDefaultParams : function() {
+
+        return {
+            unknownType : 'unknown'
+        }
+    }
+
+}, /** @lends Attach */{
+
+    live : function() {
+
+        this.liveBindTo('change', function() {
+            this._update();
+        });
+
+        if ($.browser.msie && parseInt($.browser.version) <= 8) {
+
+            var intervalId,
+                prevPath;
+
+            this.liveBindTo('click', function() {
+
+                if (typeof prevPath == 'undefined')
+                    prevPath = this.val();
+
+                var that = this;
+
+                intervalId = setInterval(function() {
+                    var path = that.val();
+                    if (prevPath != path) {
+                        clearInterval(intervalId);
+                        prevPath = path;
+                        that._update();
+                    }
+                }, 300);
+
+            });
+
+        }
+
+        return false;
+
+    }
+
+});

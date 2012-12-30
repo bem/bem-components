d0e64699e22a1306ef8cf8e48185ddd165ef5c00
Vladimir Varankin <nek.narqo+git@gmail.com>
2012-12-30 04:53:55 +0400

    Project's root level config

---
+++ .bem/level.js
@@ -0,0 +1,10 @@
+exports.baseLevelPath = require.resolve('bem/lib/levels/simple');
+
+exports.getTechs = function() {
+
+    return {
+        'blocks'    : '',
+        'bundles'   : ''
+    };
+
+};

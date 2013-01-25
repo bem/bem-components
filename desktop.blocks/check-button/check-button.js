5063292d075b4928cd73b98c6b2d0b496c6ed1f6
Elena Musatova <span4ik@yandex-team.ru>
2013-01-25 11:59:12 +0200

    check-button: Перенести блок во внешнюю библиотеку.
    Перенесла в bl-controls элементы __control,  __icon, __text.
    Перенесла в bl-controls модификаторы _checked, _disabled, _focused, _only-icon, _pressed.
    Перенесла в bl-controls модификаторы размеров _size .
    Добавила описание title.txt для _theme.
    Перенесла css и ie9.css
    Перенесла в bl-controls технологии bemhtml, deps.js, js, desc.wiki, title.txt.

---
+++ desktop.blocks/check-button/check-button.js
@@ -0,0 +1,61 @@
+/**
+ * @namespace
+ * @name CheckButton
+ */
+BEM.DOM.decl({ block: 'check-button', baseBlock: 'checkbox' }, /** @lends CheckButton.prototype */ {
+
+    onSetMod : {
+
+        'js' : function() {
+            this.__base();
+
+            this.isChecked() &&
+                this.setMod('pressed', 'yes');
+        },
+
+        'checked' : function(modName, modVal) {
+            if(this.__base.apply(this, arguments) === false)
+                return false;
+
+            this.toggleMod('pressed', 'yes', '', modVal === 'yes');
+        },
+
+        'pressed' : function(modName, modVal) {
+            if(this.isDisabled() && !this.isChecked())
+                return false;
+        }
+
+    },
+
+    _onMouseDown : function(e) {
+        e.preventDefault();
+
+        this.isChecked() || this.setMod('pressed', 'yes');
+
+        // XXX: синхронизируем состояние кнопки и чекбокса
+        this.bindToDoc('mouseup', function(e) {
+            var _this = this;
+
+            setTimeout(function() {
+                _this.isChecked() || _this.delMod('pressed');
+            }, 0);
+
+            _this.unbindFromDoc('mouseup');
+        });
+    }
+
+}, /** @lends CheckButton */ {
+
+    live : function() {
+
+        this.__base.apply(this, arguments);
+
+        this.liveBindTo('mousedown', function(e) {
+            this._onMouseDown(e);
+        });
+
+        return false;
+
+    }
+
+});

891170633265367d206adf60d7e79fc1adc5cb82
Elena Musatova <span4ik@yandex-team.ru>
2013-01-28 14:55:43 +0200

    radio: Перенести блок во внешнюю библиотеку Вынесла технологии, элементы и модификаторы блока radio в bl-controls.

---
+++ desktop.blocks/radio/__button/radio__button.deps.js
@@ -0,0 +1,11 @@
+({
+    shouldDeps: {
+        mods: {
+            'checked': 'yes',
+            'pressed': 'yes',
+            'focused': 'yes',
+            'disabled': 'yes',
+            'side': 'both'
+        }
+    }
+})

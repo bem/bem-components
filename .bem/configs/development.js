594ac16d1de60d67071fcdebae2931d241754ddc
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-02-05 22:38:08 +0400

    Use .bem/repo as repository db

--- .bem/configs/development.js
+++ .bem/configs/development.js
@@ -4,20 +4,15 @@ var relative = require('path').relative.bind(null, __dirname);
 
 module.exports = {
 
-    libraries : {
+    libraries : require('bem').util.extend(require('../repo'), {
 
         'bem-bl' : {
             type        : 'symlink',
             npmPackages : false,
             relative    : relative('bem-bl')
-        },
-        'bem-html' : {
-            type        : 'symlink',
-            npmPackages : false,
-            relative    : relative('bemhtml')
         }
 
-    }
+    })
 
 };
 

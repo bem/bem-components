6c1c531fef2205043b57c1596c16f85b95beed2b
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-02-05 22:45:24 +0400

    Use environ's API in bundles level config

--- desktop.bundles/.bem/level.js
+++ desktop.bundles/.bem/level.js
@@ -1,16 +1,22 @@
-var BEM = require('bem');
+var BEM = require('bem'),
+    environ = require('../../.bem/environ');
 
 exports.baseLevelPath = require.resolve('../../.bem/levels/bundles.js');
 
 exports.getConfig = function() {
 
     return BEM.util.extend(this.__base() || {}, {
-        bundleBuildLevels: this.resolvePaths([
-            '../../vendor/bem-bl/blocks-common',
-            '../../vendor/bem-bl/blocks-desktop',
-            '../../common.blocks',
-            '../../desktop.blocks'
-        ])
+
+        bundleBuildLevels : [
+                'blocks-common',
+                'blocks-desktop'
+            ]
+            .map(environ.getLibPath.bind(null, 'bem-bl'))
+            .concat(this.resolvePaths([
+                '../../common.blocks',
+                '../../desktop.blocks'
+            ]))
+
     });
 
 };

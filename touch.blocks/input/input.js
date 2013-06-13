6bfe5e2a79349bb6da500f02fedbb4e2a2dc948a
Sergey veged Berezhnoy <veged@mail.ru>
2013-06-14 01:11:09 +0300

    Use ymodules in `touch.blocks/input` block + review

--- touch.blocks/input/input.js
+++ touch.blocks/input/input.js
@@ -1,13 +1,14 @@
-BEM.DOM.decl('input', null, {
+modules.define('i-bem__dom', function(provide, DOM) {
 
+DOM.decl('input', null, {
     live : function() {
-
         this.liveBindTo('clear', 'pointerdown', function() {
             this._onClearClick();
         });
-
         return false;
-
     }
+});
+
+provide(DOM);
 
 });

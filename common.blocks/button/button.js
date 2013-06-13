c688a8fc1bb977de930b07e15e22f20dc9f193a8
Sergey veged Berezhnoy <veged@mail.ru>
2013-06-14 00:08:49 +0300

    Use ymodules in `button` block + review

--- common.blocks/button/button.js
+++ common.blocks/button/button.js
@@ -1,63 +1,56 @@
+modules.define('i-bem__dom', ['next-tick'], function(provide, nextTick, DOM) {
+
 /**
  * @namespace
  * @name Button
  */
-BEM.DOM.decl('button', /** @lends Button.prototype */ {
-
+DOM.decl('button', /** @lends Button.prototype */ {
     onSetMod : {
+        'js' : {
+            'inited' : function() {
+                var disabled = this.isDisabled(),
+                    domElem = this.domElem;
 
-        'js' : function() {
+                (this._href = domElem.attr('href')) && disabled &&
+                    domElem.removeAttr('href');
 
-            var disabled = this.isDisabled(),
-                domElem = this.domElem;
+                domElem.attr('disabled', disabled);
 
-            (this._href = domElem.attr('href')) && disabled &&
-                domElem.removeAttr('href');
+                this._isFocusable = 'a button'.indexOf(domElem[0].tagName.toLowerCase()) > -1;
+            },
 
-            domElem.attr('disabled', disabled);
-
-            this._isFocusable = 'a button'.indexOf(domElem[0].tagName.toLowerCase()) > -1;
+            '' : function() {
+                this.delMod('focused');
+            }
         },
 
         'focused' : {
-
             'yes' : function() {
-
-                if(this.isDisabled())
-                    return false;
+                if(this.isDisabled()) return false;
 
                 this
-                    .bindToWin('unload', function() {
-                        this.delMod('focused');
-                    })
+                    .bindToWin('unload', function() { this.delMod('focused') })
                     .bindTo('keydown', this._onKeyDown);
 
                 this._isFocusable && (this._isControlFocused() || this.domElem.focus());
 
-                this.afterCurrentEvent(function() {
-                    this.trigger('focus');
-                });
-
+                // TODO: заменить на `this.nextTick` после https://github.com/bem/bem-core/issues/71
+                nextTick(function() { this.trigger('focus') }.bind(this)); // TODO: убрать bind после https://github.com/bem/bem-core/issues/70
             },
 
             '' : function() {
-
                 this
                     .unbindFromWin('unload')
                     .unbindFrom('keydown');
 
                 this._isFocusable && this._isControlFocused() && this.domElem.blur();
 
-                this.afterCurrentEvent(function() {
-                    this.trigger('blur');
-                });
-
+                // TODO: заменить на `this.nextTick` после https://github.com/bem/bem-core/issues/71
+                nextTick(function() { this.trigger('blur') }.bind(this)); // TODO: убрать bind после https://github.com/bem/bem-core/issues/70
             }
-
         },
 
         'disabled' : function(modName, modVal) {
-
             var disable = modVal == 'yes',
                 domElem = this.domElem;
 
@@ -65,39 +58,29 @@ BEM.DOM.decl('button', /** @lends Button.prototype */ {
                 domElem.removeAttr('href') :
                 domElem.attr('href', this._href));
 
+            // TODO: нужно более явно отжимать кнопку,
+            // а не создавать фейковое событие keyup,
+            // которое кто-то может слушать и использовать
             disable && domElem.keyup();
 
-            this.afterCurrentEvent(function() {
-                domElem.attr('disabled', disable);
-            });
-
+            domElem.attr('disabled', disable);
         },
 
         'pressed' : function(modName, modVal) {
-
             this.setMod('focused', 'yes');
-
             this.isDisabled() || this.trigger(modVal == 'yes' ? 'press' : 'release');
-
         },
 
         'hovered' : {
-
             '' : function() {
-
                 this.delMod('pressed');
-
             }
-
         },
 
         '*' : function(modName) {
-
             if(this.isDisabled() && 'hovered pressed'.indexOf(modName) > -1)
                 return false;
-
         }
-
     },
 
     /**
@@ -105,9 +88,7 @@ BEM.DOM.decl('button', /** @lends Button.prototype */ {
      * @returns {Boolean}
      */
     isDisabled : function() {
-
         return this.hasMod('disabled', 'yes');
-
     },
 
     /**
@@ -115,15 +96,13 @@ BEM.DOM.decl('button', /** @lends Button.prototype */ {
      * @param {String} [val] урл
      */
     url : function(val) {
-
-        if(typeof val == 'undefined') {
-            return this._href;
-        } else {
+        if(arguments.length) {
             this._href = val;
             this.isDisabled() || this.domElem.attr('href', val);
             return this;
+        } else {
+            return this._href;
         }
-
     },
 
     /**
@@ -132,17 +111,14 @@ BEM.DOM.decl('button', /** @lends Button.prototype */ {
      * @returns {Boolean}
      */
     _isControlFocused : function() {
-
         try {
-            return this.containsDomElem($(document.activeElement));
+            return this.containsDomElem(DOM.doc.activeElement);
         } catch(e) {
             return false;
         }
-
     },
 
     _onKeyDown : function(e) {
-
         var keyCode = e.keyCode;
         // имитируем state_pressed по нажатию на enter и space
         if((keyCode == 13 || keyCode == 32) && !this._keyDowned) {
@@ -157,33 +133,20 @@ BEM.DOM.decl('button', /** @lends Button.prototype */ {
                     delete this._keyDowned;
 
                     // делаем переход по ссылке по space
-                    if(keyCode == 32 && this.domElem.attr('href')) {
-                        document.location = this.domElem.attr('href');
-                    }
+                    keyCode == 32 && this.domElem.attr('href') &&
+                        (document.location = this.domElem.attr('href'));
                 });
         }
-
     },
 
     _onClick : function(e) {
         this.isDisabled()?
             e.preventDefault() :
-            this.afterCurrentEvent(function() {
-                this.trigger('click');
-            });
-    },
-
-    destruct : function () {
-
-        this.delMod('focused');
-        this.__base.apply(this, arguments);
-
+            // TODO: заменить на `this.nextTick` после https://github.com/bem/bem-core/issues/71
+            nextTick(function() { this.trigger('click') }.bind(this)); // TODO: убрать bind после https://github.com/bem/bem-core/issues/70
     }
-
 }, /** @lends Button */ {
-
     live : function() {
-
         var eventsToMods = {
             'mouseover' : { name : 'hovered', val : 'yes' },
             'mouseout' : { name : 'hovered' },
@@ -194,9 +157,7 @@ BEM.DOM.decl('button', /** @lends Button.prototype */ {
         };
 
         this
-            .liveBindTo('leftclick', function(e) {
-                this._onClick(e);
-            })
+            .liveBindTo('leftclick', function(e) { this._onClick(e) })
             .liveBindTo('mouseover mouseout mouseup focusin focusout', function(e) {
                 var mod = eventsToMods[e.type];
                 this.setMod(mod.name, mod.val || '');
@@ -209,5 +170,8 @@ BEM.DOM.decl('button', /** @lends Button.prototype */ {
                 this._isControlFocused() && e.preventDefault();
             });
     }
+});
+
+provide(DOM);
 
 });

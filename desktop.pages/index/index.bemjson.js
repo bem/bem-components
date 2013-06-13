2df19a627c85d33e495ae5beba03b510613998f5
Sergey veged Berezhnoy <veged@mail.ru>
2013-06-13 21:27:57 +0300

    Use `page` instead of `b-page`

--- desktop.pages/index/index.bemjson.js
+++ desktop.pages/index/index.bemjson.js
@@ -1,14 +1,11 @@
 ({
-    block: 'b-page',
+    block: 'page',
     title: 'Title of the page',
     favicon: '/favicon.ico',
     head: [
         { elem: 'css', url: '_index.css', ie: false },
         { elem: 'css', url: '_index', ie: true },
-        { block: 'i-jquery', elem: 'core' },
         { elem: 'js', url: '_index.js' },
-        { elem: 'meta', attrs: { name: 'description', content: '' } },
-        { elem: 'meta', attrs: { name: 'keywords', content: '' } }
     ],
     content:[
         {

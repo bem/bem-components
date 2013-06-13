540e25ecda22f36a49f83cfd0c4df53501f8f171
Sergey veged Berezhnoy <veged@mail.ru>
2013-06-14 01:09:40 +0300

    Use ymodules in `touch-pad.blocks/radiobox` block + review

--- touch-pad.blocks/radiobox/radiobox.js
+++ touch-pad.blocks/radiobox/radiobox.js
@@ -1,8 +1,8 @@
-BEM.DOM.decl('radiobox', null, {
+modules.define('i-bem__dom', function(provide, DOM) {
 
+DOM.decl('radiobox', null, {
     live : function() {
-
-    	this.__base.apply(this, arguments);
+        this.__base.apply(this, arguments);
 
         this.liveBindTo('radio', 'tap', function(e) {
             this._onLeftClick(e);
@@ -10,6 +10,9 @@ BEM.DOM.decl('radiobox', null, {
         });
 
         return false;
-
     }
+});
+
+provide(DOM);
+
 });

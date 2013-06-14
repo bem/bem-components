7f103dc90acf27f4b95b1aa4eb1dfb16dc91b2d5
Sergey veged Berezhnoy <veged@mail.ru>
2013-06-14 21:19:57 +0300

    Use ymodules and js-syntax for BEMHTML in `radiobox` + review

--- desktop.pages/index/index.bemjson.js
+++ desktop.pages/index/index.bemjson.js
@@ -7,56 +7,79 @@
         { elem: 'css', url: '_index', ie: true },
         { elem: 'js', url: '_index.js' },
     ],
-    content:[
-        {
-            tag: 'form',
-            attrs: { action: 'http://example.com' },
-            content: [
-                {
-                    block: 'button',
-                    mods: { size: 'm' },
-                    content: 'Custom Button'
-                },
-                {
-                    block: 'attach',
-                    mods: { size: 's' },
-                    content: [
-                        {
-                            block: 'button',
-                            mods: { size: 's' },
-                            mix: [{ block: 'attach', elem: 'button' }],
-                            tabindex: 1,
-                            id: 'bla',
-                            content: 'button text'
-                        },
-                        {
-                            elem: 'holder',
-                            content: 'no file'
-                        }
-                    ]
-                },
-                { block: 'checkbox', mods: { size: 's', checked: 'yes' } },
-                {
-                    block: 'check-button',
-                    name: 'porn',
-                    value: '1',
-                    mods: { size: 's' },
-                    content: '1'
-                },'&nbsp;&nbsp;&nbsp;',
-                {
-                    block: 'check-button',
-                    name: 'porn',
-                    mods: { size: 's' },
-                    content: '2'
-                },
-                { tag: 'br' },
-                {
-                    block: 'input',
-                    mods: { size: 's', autofocus: 'focused' },
-                    content: { elem: 'control' }
-                },
-                { tag: 'br' }
-            ]
-        }
-    ]
+    content: {
+        tag: 'form',
+        attrs: { action: 'http://example.com' },
+        content: [
+            {
+                block: 'button',
+                mods: { size: 'm' },
+                content: 'Custom Button'
+            },
+            {
+                block: 'attach',
+                mods: { size: 's' },
+                content: [
+                    {
+                        block: 'button',
+                        mods: { size: 's' },
+                        mix: [{ block: 'attach', elem: 'button' }],
+                        tabindex: 1,
+                        id: 'bla',
+                        content: 'button text'
+                    },
+                    {
+                        elem: 'holder',
+                        content: 'no file'
+                    }
+                ]
+            },
+            { block: 'checkbox', mods: { size: 's', checked: 'yes' } },
+            {
+                block: 'check-button',
+                name: 'porn',
+                value: '1',
+                mods: { size: 's' },
+                content: '1'
+            },'&nbsp;&nbsp;&nbsp;',
+            {
+                block: 'check-button',
+                name: 'porn',
+                mods: { size: 's' },
+                content: '2'
+            },
+            { tag: 'br' },
+            {
+                block: 'input',
+                mods: { size: 's', autofocus: 'focused' },
+                content: { elem: 'control' }
+            },
+            { tag: 'br' },
+            {
+                block: 'radiobox',
+                mods: { size: 's' },
+                name: 'bla',
+                content: [
+                    {
+                        elem: 'radio',
+                        content: 'только друзьям',
+                        controlAttrs: { value: 'val-1' }
+                    },
+                    '&nbsp;&nbsp;&nbsp;&nbsp;',
+                    {
+                        elem: 'radio',
+                        content: 'только мне',
+                        controlAttrs: { value: 'val-2' }
+                    },
+                    '&nbsp;&nbsp;&nbsp;&nbsp;',
+                    {
+                        elem: 'radio',
+                        elemMods: { disabled: 'yes' },
+                        content: 'только не мне',
+                        controlAttrs: { value: 'val-3' }
+                    }
+                ]
+            }
+        ]
+    }
 })

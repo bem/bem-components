fe988b18a13c959c2fce66d36d80107465a411b3
Dmitry Filatov <dfilatov@yandex-team.ru>
2013-07-05 14:30:56 +0400

    use test.js+browser.js+bemhtml from bem-core

--- .bem/nodes/bundle.js
+++ .bem/nodes/bundle.js
@@ -3,11 +3,3 @@ var environ = require('../environ');
 try {
     require(environ.getLibPath('bem-core', '.bem/nodes/bundle.js'));
 } catch(e) {}
-
-require('bem/lib/nodesregistry').decl('BundleNode', {
-
-    'create-test.js+browser.js+bemhtml-optimizer-node': function(tech, sourceNode, bundleNode) {
-        return this.createBorschikOptimizerNode('js', sourceNode, bundleNode);
-    }
-
-});

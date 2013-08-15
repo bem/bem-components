ce3c63130af759fd803644b149e7cd7fcdcdee28
Dmitry Filatov <dfilatov@yandex-team.ru>
2013-08-15 14:08:10 +0400

    attach: bem-structure refactoring

--- common.blocks/attach/attach.deps.js
+++ common.blocks/attach/attach.deps.js
@@ -1,22 +1,23 @@
 [
 {
-    mustDeps: { block: 'i-bem', elems: ['dom', 'i18n'] },
-    shouldDeps: [
-        { block: 'button' },
-        // TODO: непонятно почему мы зависим от блока, которого даже нет в этой библиотеке
-        { block: 'b-icon' },
-        { elem: 'holder', mods: { state: 'hidden' } },
-        { elem: 'reset', mods: { visibility: 'visible' } },
-        { elems: [ 'icon-file', 'button', 'control' ] }
+    mustDeps : { block : 'i-bem', elems : ['dom', 'i18n'] },
+    shouldDeps : [
+        {
+            block : 'button',
+            mods : { 'only-icon'  : 'yes' }// TODO : убрать после https ://github.com/bem/bem-tools/pull/424
+        },
+        { elem : 'holder', mods : { state : 'hidden' } },
+        { elem : 'reset', mods : { visibility : 'visible' } },
+        { elems : ['text', 'icon-file', 'button', 'control'] }
     ]
 },
 {
-    tech: 'js',
-    shouldDeps: [
-        // TODO: нужно что-то придумать с повторением `tech: 'bemhtml'`
-        { tech: 'bemhtml', block: 'i-bem' },
-        // TODO: почему-то не работает без указания `block: 'attach'`
-        { tech: 'bemhtml', block: 'attach', elem: 'control' }
+    tech : 'js',
+    shouldDeps : [
+        // TODO : нужно что-то придумать с повторением `tech : 'bemhtml'`
+        { tech : 'bemhtml', block : 'i-bem' },
+        // TODO : почему-то не работает без указания `block : 'attach'`
+        { tech : 'bemhtml', block : 'attach', elem : 'control' }
     ]
 }
 ]

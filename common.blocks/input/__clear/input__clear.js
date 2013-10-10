aa37a29286758a834accf494b2c00e5edfd551d4
Dmitriy Lisitsyn <cyn@yandex-team.ru>
2013-10-10 12:08:47 +0300

    ISLCOMPONENTS-365 - input: Проверка необходимости показа очищающего крестика и подсказки в bemhtml, небольшой рефакторинг js

--- common.blocks/input/__clear/input__clear.js
+++ common.blocks/input/__clear/input__clear.js
@@ -7,55 +7,21 @@ BEM.DOM.decl('input', /** @lends Block.prototype */ {
     onSetMod : {
 
         'js' : function() {
-
             this.__base.apply(this, arguments);
 
-            var _this = this;
-
-            if(_this.elem('clear') && !_this.hasMod('clear', 'visibility', 'visible')) {
-                _this.bindTo('box', 'click', function(e) {
-                    _this.setMod('focused', 'yes');
-                });
-            }
-
-            _this.bindInput();
+            this
+                .on('change', this._updateClear)
+                ._updateClear(); // на случай автокомплита
         }
     },
 
-    onElemSetMod : {
-
-        'clear' : {
-
-            'visibility' : {
-
-                'visible' : function() {
-                    this.unbindFrom('box', 'click');
-                },
-
-                '' : function() {
-                    var _this = this;
-                    this.bindTo('box', 'click', function(e) {
-                        _this.setMod('focused', 'yes');
-                    });
-                }
-            }
-
-        }
-    },
-
-    bindInput : function() {
-
-        var _this = this;
-
-        _this.bindTo('input', function() {
-            _this.val(_this.elem('control').val());
-            _this._updateClear();
-        });
-
-        _this
-            .on('change', _this._updateClear)
-            ._updateClear();
-
+    /**
+     * Обработчик клика по элементу box
+     * Переводит фокус на контрол при клике на место очищаего крестика, когда его не видно
+     * @private
+     */
+    _onBoxClear: function() {
+        this.hasMod('clear', 'visibility', 'visible') || this.setMod('focused', 'yes');
     },
 
     /**
@@ -65,7 +31,6 @@ BEM.DOM.decl('input', /** @lends Block.prototype */ {
      * @return {Object} экземпляр блока input
      */
     _onClearClick : function() {
-
         this.trigger('clear');
         this.removeInsets && this.removeInsets();
 
@@ -76,13 +41,9 @@ BEM.DOM.decl('input', /** @lends Block.prototype */ {
     },
 
     clearInput : function(data) {
-
-        this.elem('control').val('');
-        this._val = '';
-        this.trigger('change', data);
+        this.val('', data);
 
         return this;
-
     },
 
     /**
@@ -92,22 +53,22 @@ BEM.DOM.decl('input', /** @lends Block.prototype */ {
      * @return {Object} экземпляр блока input
      */
     _updateClear : function() {
-
         return this.toggleMod(this.elem('clear'), 'visibility', 'visible', '', !!this._val);
-
     }
 
 }, {
 
     live : function() {
-
         this.__base.apply(this, arguments);
 
-        this.liveBindTo('clear', 'leftclick', function(e) {
-            this._onClearClick();
-        });
+        this
+            .liveBindTo('clear', 'leftclick', function(e) {
+                this._onClearClick();
+            })
+            .liveBindTo('box', 'click', function(e) {
+                this._onBoxClear(e);
+            });
 
         return false;
-
     }
 });

4e5a16635e09821e72422b1f48cf824a8e4b0fc8
Mikhail Troshev <mishanga@yandex-team.ru>
2013-11-15 15:15:05 +0400

    Update index page (#232)

--- desktop.pages/.bem/level.js
+++ desktop.pages/.bem/level.js
@@ -1,18 +1,1 @@
-var BEM = require('bem'),
-    environ = require('bem-environ');
-
 exports.baseLevelPath = require.resolve('../../.bem/levels/bundles');
-
-exports.getConfig = function() {
-
-    return BEM.util.extend(this.__base() || {}, {
-
-        bundleBuildLevels : [
-            environ.getLibPath('bem-core', 'common.blocks'),
-            this.resolvePath('../../common.blocks'),
-            this.resolvePath('../../desktop.blocks')
-        ]
-
-    });
-
-};

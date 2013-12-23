3e7ee1659702af7a34ca86f0ec962c6a30471e39
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-12-24 00:30:21 +0400

    Add browsers settings to make.js
    use `ie 10` and `opera 12.16` for desktop

--- .bem/make.js
+++ .bem/make.js
@@ -4,8 +4,8 @@ var PATH = require('path'),
     environ = require('bem-environ')(__dirname);
 
 environ.extendMake(MAKE);
+require('bem-tools-autoprefixer').extendMake(MAKE);
 
-require('bem-tools-autoprefixer')(MAKE);
 require('./nodes')(MAKE);
 
 try {
@@ -119,6 +119,45 @@ MAKE.decl('BundleNode', {
         if(this.ctx.arch.hasNode(source)) {
             return this.createAutoprefixerNode(tech, this.ctx.arch.getNode(source), bundleNode, magicNode);
         }
+    }
+
+});
+
+
+MAKE.decl('AutoprefixerNode', {
+
+    getPlatform : function() {
+        return this.output.split('.')[0];
+    },
+
+    getBrowsers : function() {
+        var platform = this.getPlatform();
+        switch(platform) {
+
+        case 'desktop':
+            return [
+                'last 2 versions',
+                'ie 10',
+                'ff 24',
+                'opera 12.16'
+            ];
+
+        case 'touch-pad':
+            return [
+                'android 4',
+                'ios 5'
+            ];
+
+        case 'touch-phone':
+            return [
+                'android 4',
+                'ios 6',
+                'ie 10'
+            ];
+
+        }
+
+        return this.__base();
     }
 
 });

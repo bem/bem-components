e68ae45f0b19935e3035a7d2f85bade6183d5bb6
Mikhail Troshev <mishanga@yandex-team.ru>
2014-01-10 15:58:15 +0400

    New pages for three devices: desktop, tablet and phone

---
+++ desktop.pages/normal/normal.bemjson.js
@@ -0,0 +1,180 @@
+({
+    block : 'page',
+    title : 'bem-components: normal theme',
+    mods : { theme : 'normal' },
+    head : [
+        { elem : 'css', url : '_normal.css' },
+        { elem : 'js', url : '_normal.js' }
+    ],
+    content : [
+        { tag : 'h2', content : 'Normal theme' },
+
+        'size s (24px height) ',
+        {
+            block : 'input',
+            mods : { theme : 'normal', size : 's', 'has-clear' : true },
+            val : 'value',
+            placeholder : 'placeholder'
+        },
+        ' ',
+        {
+            block : 'button',
+            mods : { theme : 'normal', size : 's' },
+            text : 'button'
+        },
+        ' ',
+        {
+            block : 'radio',
+            mods : { theme : 'normal', size : 's', type : 'button' },
+            name : 'radio-sizes-s',
+            options : [
+                { val : 1, text : 'first' },
+                { val : 2, text : 'second' }
+            ],
+            val : 2
+        },
+        ' ',
+        {
+            block : 'checkbox',
+            mods : { theme : 'normal', size : 's', type : 'button' },
+            val : 1,
+            text : 'check'
+        },
+
+        { tag : 'br' },
+        { tag : 'br' },
+
+        'size m (28px height) ',
+        {
+            block : 'input',
+            mods : { theme : 'normal', size : 'm', 'has-clear' : true },
+            val : 'value',
+            placeholder : 'placeholder'
+        },
+        ' ',
+        {
+            block : 'button',
+            mods : { theme : 'normal', size : 'm' },
+            text : 'button'
+        },
+        ' ',
+        {
+            block : 'radio',
+            mods : { theme : 'normal', size : 'm', type : 'button' },
+            name : 'radio-sizes-m',
+            options : [
+                { val : 1, text : 'first' },
+                { val : 2, text : 'second' }
+            ],
+            val : 2
+        },
+        ' ',
+        {
+            block : 'checkbox',
+            mods : { theme : 'normal', size : 'm', type : 'button' },
+            val : 1,
+            text : 'check'
+        },
+
+        { tag : 'br' },
+        { tag : 'br' },
+
+        'size l (32px height) ',
+        {
+            block : 'input',
+            mods : { theme : 'normal', size : 'l', 'has-clear' : true },
+            val : 'value',
+            placeholder : 'placeholder'
+        },
+        ' ',
+        {
+            block : 'button',
+            mods : { theme : 'normal', size : 'l' },
+            text : 'button'
+        },
+        ' ',
+        {
+            block : 'radio',
+            mods : { theme : 'normal', size : 'l', type : 'button' },
+            name : 'radio-sizes-l',
+            options : [
+                { val : 1, text : 'first' },
+                { val : 2, text : 'second' }
+            ],
+            val : 2
+        },
+        ' ',
+        {
+            block : 'checkbox',
+            mods : { theme : 'normal', size : 'l', type : 'button' },
+            val : 1,
+            text : 'check'
+        },
+
+        { tag : 'br' },
+        { tag : 'br' },
+
+        'size xl (36px height) ',
+        {
+            block : 'input',
+            mods : { theme : 'normal', size : 'xl', 'has-clear' : true },
+            val : 'value',
+            placeholder : 'placeholder'
+        },
+        ' ',
+        {
+            block : 'button',
+            mods : { theme : 'normal', size : 'xl' },
+            text : 'button'
+        },
+        ' ',
+        {
+            block : 'radio',
+            mods : { theme : 'normal', size : 'xl', type : 'button' },
+            name : 'radio-sizes-xl',
+            options : [
+                { val : 1, text : 'first' },
+                { val : 2, text : 'second' }
+            ],
+            val : 2
+        },
+        ' ',
+        {
+            block : 'line',
+            mods : { size : 'xl' },
+            content : [
+                'size xl (38px height) ',
+                {
+                    block : 'input',
+                    mods : { theme : 'normal', size : 'xl', 'has-clear' : true },
+                    val : 'value',
+                    placeholder : 'placeholder'
+                },
+                ' ',
+                {
+                    block : 'button',
+                    mods : { theme : 'normal', size : 'xl' },
+                    text : 'button'
+                },
+                ' ',
+                {
+                    block : 'radio',
+                    mods : { theme : 'normal', size : 'xl', type : 'button' },
+                    name : 'radio-sizes-xl',
+                    options : [
+                        { val : 1, text : 'first' },
+                        { val : 2, text : 'second' }
+                    ],
+                    val : 2
+                },
+                ' ',
+                {
+                    block : 'checkbox',
+                    mods : { theme : 'normal', size : 'xl', type : 'button' },
+                    val : 1,
+                    text : 'check'
+                }
+            ]
+        }
+    ]
+})

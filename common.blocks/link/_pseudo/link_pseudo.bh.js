a2dde7c5427e010c259df893b61d1ed61a2b3594
Mikhail Troshev <mishanga@yandex-team.ru>
2014-03-17 23:51:11 +0400

    link: Normal theme

--- common.blocks/link/_pseudo/link_pseudo.bh.js
+++ common.blocks/link/_pseudo/link_pseudo.bh.js
@@ -1,5 +1,6 @@
 module.exports = function(bh) {
-    bh.match('link_pseudo', function(ctx) {
+    bh.match('link_pseudo', function(ctx, json) {
         ctx.js(true);
+        json.url || ctx.tag('span');
     });
 };

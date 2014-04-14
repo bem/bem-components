f1b9ef7aee4632dae7ea8df83515d8462f82d477
andrewblond <andrewblond@yandex.com>
2014-04-14 14:47:21 +0400

    Used enb-islands-tools

---
+++ .enb/make.js
@@ -0,0 +1,59 @@
+module.exports = function(config) {
+    var tools = require('enb-islands-tools')(config);
+
+    config.setLanguages(['en', 'ru']);
+
+    tools.configureSets(getDesktopSetsLevels(config), {
+        destPath: 'desktop.sets',
+        levels: getDesktopLevels(config),
+        examples: {
+            directorySuffixes: ['tests'],
+            techs: [
+                require('enb-roole/techs/css-roole'),
+                [ require('enb-diverse-js/techs/browser-js'), { target: '?.js' } ],
+                require('enb-bemxjst/techs/bemhtml'),
+                require('enb/techs/html-from-bemjson')
+            ],
+            targets: [
+                '?.css', '?.js',
+                '?.bemhtml.js', '?.html'
+            ],
+            optimizeTargets: [
+                '?.css',
+                '?.js'
+            ]
+        }
+    });
+};
+
+/**
+ * Получение уровней c примерами
+ * @param {Object} config
+ * @returns {*|Array}
+ */
+function getDesktopSetsLevels(config) {
+    return [
+        'common.blocks',
+        'desktop.blocks'
+    ].map(function(level) {
+        return config.resolvePath(level);
+    });
+}
+
+
+/**
+ * Получение уровней для сборки примеров
+ * @param {Object} config
+ * @returns {*|Array}
+ */
+function getDesktopLevels(config) {
+    return [
+        { path: 'libs/bem-core/common.blocks', check: false },
+        { path: 'libs/bem-core/desktop.blocks', check: false },
+        'common.blocks',
+        'desktop.blocks',
+        'design/common.blocks'
+    ].map(function(level) {
+        return config.resolvePath(level);
+    });
+}

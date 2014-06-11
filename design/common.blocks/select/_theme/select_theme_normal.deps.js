d03766d4cccff03736a5ccdc238617e64c3dabcd
Mikhail Troshev <mishanga@yandex-team.ru>
2014-06-11 11:11:37 +0400

    select: make css (close #568)

--- design/common.blocks/select/_theme/select_theme_normal.deps.js
+++ design/common.blocks/select/_theme/select_theme_normal.deps.js
@@ -2,6 +2,7 @@
     shouldDeps : [
         { block : 'popup', mods : { theme : 'normal' } },
         { block : 'menu', mods : { theme : 'normal' } },
-        { block : 'button', mods : { theme : 'normal' } }
+        { block : 'button', mods : { theme : 'normal' } },
+        { block : 'icon' }
     ]
 })

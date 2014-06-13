3c43d7e08ed050f98d5957d9f98c1e3259de20ba
Petr Lapin <invazia@mail.ru>
2014-06-13 13:44:05 +0300

    fixed setUrl

---
+++ common.blocks/spin/spin.tests/gemini.bemjson.js
@@ -0,0 +1,60 @@
+({
+    block : 'page',
+    title : 'bem-components: spin',
+    mods : { theme : 'normal' },
+    head : [
+        { elem : 'css', url : '_gemini.css' }
+    ],
+    content : [
+        { tag : 'h2', content : 'simple' },
+        { tag : 'p', content : {
+            block : 'spin',
+            mods : { paused : true, theme : 'simple', size : 's', progress : true }
+        } },
+
+        { tag : 'hr' },
+
+        { tag : 'h2', content : 'normal' },
+        { tag : 'h3', content : 'size' },
+        { tag : 'p', content : [
+            'xs ',
+            {
+                block : 'spin',
+                mods : { paused : true, theme : 'normal', size : 'xs', progress : true },
+                cls : 'gemini-test-spin-xs'
+            }
+        ] },
+        { tag : 'p', content : [
+            's ',
+            {
+                block : 'spin',
+                mods : { paused : true, theme : 'normal', size : 's', progress : true },
+                cls : 'gemini-test-spin-s'
+            }
+        ] },
+        { tag : 'p', content : [
+            'm ',
+            {
+                block : 'spin',
+                mods : { paused : true, theme : 'normal', size : 'm', progress : true },
+                cls : 'gemini-test-spin-m'
+            }
+        ] },
+        { tag : 'p', content : [
+            'l ',
+            {
+                block : 'spin',
+                mods : { paused : true, theme : 'normal', size : 'l', progress : true },
+                cls : 'gemini-test-spin-l'
+            }
+        ] },
+        { tag : 'p', content : [
+            'xl ',
+            {
+                block : 'spin',
+                mods : { paused : true, theme : 'normal', size : 'xl', progress : true },
+                cls : 'gemini-test-spin-xl'
+            }
+        ] }
+    ]
+});
--- gemini/spin.gemini.js
+++ gemini/spin.gemini.js
@@ -1,7 +1,7 @@
 var gemini = require('gemini');
 
 gemini.suite('spin', function(root) {
-	root.setUrl('desktop.tests/spin/simple/simple.html');
+	root.setUrl('desktop.tests/spin/gemini/gemini.html');
 
 	['gemini-test-spin-xs', 'gemini-test-spin-s', 'gemini-test-spin-m', 'gemini-test-spin-l', 'gemini-test-spin-xl']
 		.forEach(function(test) {

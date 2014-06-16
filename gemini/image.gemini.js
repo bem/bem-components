a01bce306cd24a7c576f257cab13fcc40a83fa6e
Petr Lapin <invazia@mail.ru>
2014-06-16 11:22:14 +0300

    write gemini test: Image

---
+++ gemini/image.gemini.js
@@ -0,0 +1,18 @@
+var gemini = require('gemini');
+
+gemini.suite('image', function(root) {
+
+	root.setUrl('desktop.tests/image/gemini/gemini.html');
+
+	['image-nosize', 'image-size']
+		.forEach(function(test) {
+			var imageSelector = '.' + test;
+
+			gemini.suite(test, function(suite) {
+				suite
+				    .setCaptureElements(imageSelector)
+				    .capture('plain');
+			});
+		});
+		
+});

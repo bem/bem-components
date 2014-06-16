cc452f5425aa85b2fdc10ef3301689ae805c493c
Petr Lapin <invazia@mail.ru>
2014-06-16 09:29:19 +0300

    remove title suite

--- gemini/link.gemini.js
+++ gemini/link.gemini.js
@@ -1,13 +1,10 @@
 var gemini = require('gemini');
 
 gemini.suite('link', function(root) {
-    var linkSelector = '.gemini-test-link';
     root.setUrl('desktop.tests/link/gemini/gemini.html');
 
     ['normal-no-url', 'normal-url', 'normal-pseudo']
         .forEach(function(test) {
-            // var thisLinkThemeSelector = linkSelector + '-' + test,
-            //     thisLinkThemeSelectoriSPseudo = thisLinkThemeSelector + '-pseudo'; 
             var thisLinkThemeSelector = '.' + test;
 
             gemini.suite(test, function(suite) {
@@ -20,36 +17,15 @@ gemini.suite('link', function(root) {
                     .capture('plain')
                     .capture('hovered', function(actions) {
                         actions.mouseMove(this.link);
-                        actions.wait(300);
                     })
                     .capture('pressed', function(actions) {
                         actions.mouseDown(this.link);
                     })
-                    .capture('focused', function(actions) {
-                        actions.sendKeys(find('.page'), gemini.TAB);
+
+                    .capture('focused', function(actions, find) {
+                        actions.sendKeys(this.link, gemini.TAB);
                     });
             });
 
         }); 
-
-    gemini.suite('normal-title', function(suite) {
-        suite
-            .setCaptureElements('.page')
-            .before(function(actions, find) {
-                this.link = find('.normal-title');
-            })
-
-            .capture('plain')
-            .capture('hovered', function(actions) {
-                actions.mouseMove(this.link);
-                actions.wait(5000);
-            })
-            .capture('pressed', function(actions) {
-                actions.mouseDown(this.link);
-            })
-            .capture('focused', function(actions) {
-                actions.sendKeys(this.link, gemini.TAB);
-            });
-            
-    });
 });

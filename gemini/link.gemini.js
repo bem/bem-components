3285c77e3cbbcd76f4d7e85945147adf8521b8a7
Petr Lapin <invazia@mail.ru>
2014-06-24 12:57:08 +0300

    link: Gemini write test

---
+++ gemini/link.gemini.js
@@ -0,0 +1,42 @@
+var gemini = require('gemini');
+
+gemini.suite('link', function(root) {
+
+    root.setUrl('desktop.tests/link/gemini/gemini.html');
+
+    [
+        'default-no-url',
+        'default-url',
+        'default-pseudo',
+        'normal-no-url',
+        'normal-url',
+        'normal-pseudo'
+    ]
+        .forEach(function(test) {
+            var linkSelector = '.' + test;
+
+            gemini.suite(test, function(suite) {
+                suite
+                    .setCaptureElements(linkSelector)
+                    .before(function(actions, find) {
+                        this.link = find(linkSelector);
+                    })
+                    .capture('plain')
+                    .capture('hovered', function(actions) {
+                        actions
+                            .mouseMove(this.link)
+                            .wait(200);
+                    })
+                    .capture('pressed', function(actions) {
+                        actions.mouseDown(this.link);
+                    })
+                    .capture('clicked', function(actions) {
+                        actions.mouseUp(this.link);
+                    })
+                    .capture('focused', function(actions) {
+                        actions.mouseMove(this.link, { x : 100, y : 0 });
+                    });
+            });
+        });
+
+});

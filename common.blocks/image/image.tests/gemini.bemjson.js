10d0fc036b47cf74ba376829d9bc4fb9d2344952
gela <gela-d@yandex-team.ru>
2014-06-26 11:01:52 +0400

    image: Write gemini test

---
+++ common.blocks/image/image.tests/gemini.bemjson.js
@@ -0,0 +1,23 @@
+({
+    block : 'page',
+    title : 'bem-components: image',
+    mods : { theme : 'normal' },
+    head : [
+        { elem : 'css', url : '_gemini.css' }
+    ],
+    content : [
+        {
+            block : 'image',
+            url : '../../../test.blocks/icon/_action/download.svg',
+            cls : 'image-no-size'
+        },
+        { tag : 'br' },
+        {
+            block : 'image',
+            url : '../../../test.blocks/icon/_action/download.svg',
+            width : 50,
+            height : 50,
+            cls : 'image-with-size'
+        }
+    ]
+});

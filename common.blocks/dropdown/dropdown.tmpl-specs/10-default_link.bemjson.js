49ac893ad6a7e0824e9894bfb06ebc04f75baeea
DimaChervan <chervan@yandex-team.ru>
2014-07-08 22:59:58 +0400

    dropdown: Write tests on templates

---
+++ common.blocks/dropdown/dropdown.tmpl-specs/10-default_link.bemjson.js
@@ -0,0 +1,6 @@
+({
+    block : 'dropdown',
+    mods : { switcher : 'link', theme : 'normal', disabled : true },
+    switcher : 'link',
+    popup : 'bemjson'
+});
---
+++ common.blocks/dropdown/dropdown.tmpl-specs/20-default_button.bemjson.js
@@ -0,0 +1,6 @@
+({
+    block : 'dropdown',
+    mods : { switcher : 'button', theme : 'normal', size : 'm', disabled : true },
+    switcher : 'button',
+    popup : 'bemjson'
+});

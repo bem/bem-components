f99388b5cf114f39595b25ba026a7ab77e9bb86d
DimaChervan <chervan@yandex-team.ru>
2014-07-08 22:59:58 +0400

    dropdown: Write tests on templates

---
+++ common.blocks/dropdown/dropdown.tmpl-specs/30-switcher_link_custom_popup.bemjson.js
@@ -0,0 +1,6 @@
+({
+    block : 'dropdown',
+    mods : { switcher : 'link' },
+    switcher : { block : 'link', mods : { theme : 'normal', disabled : true }, content : 'link' },
+    popup : { block : 'link', mods : { theme : 'normal' }, content : 'link' }
+});

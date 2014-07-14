e41396f974287974073ea59b8695cabc450e8a5f
DimaChervan <chervan@yandex-team.ru>
2014-07-14 10:57:22 +0400

    radio-group: Write tests on templates

---
+++ common.blocks/radio-group/radio-group.tmpl-specs/10-no_type_disabled_item.bemjson.js
@@ -0,0 +1,9 @@
+({
+    block : 'radio-group',
+    mods : { theme : 'normal', size : 'm' },
+    name : 'default',
+    options : [
+        { val : 1, text : 'first' },
+        { val : 3, text : 'third', disabled : true, checked : true }
+    ]
+});
---
+++ common.blocks/radio-group/radio-group.tmpl-specs/20-type_button_disabled_group.bemjson.js
@@ -0,0 +1,8 @@
+({
+    block : 'radio-group',
+    mods : { type : 'button', disabled : true },
+    options : [
+        { text : 'first' },
+        { text : 'VK', icon : { block : 'icon', mods : { social : 'vk' } } }
+    ]
+});

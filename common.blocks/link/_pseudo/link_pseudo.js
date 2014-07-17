387a75d932894f8475cc84f532b76cc4ad2f5667
DimaChervan <chervan@yandex-team.ru>
2014-07-17 15:11:12 +0400

    link: Check jsdoc

--- common.blocks/link/_pseudo/link_pseudo.js
+++ common.blocks/link/_pseudo/link_pseudo.js
@@ -1,6 +1,15 @@
+/**
+ * @module link
+ */
+
 modules.define('link', function(provide, Link) {
 
-provide(Link.decl({ modName : 'pseudo', modVal : true }, {
+/**
+ * @exports
+ * @class link
+ * @bem
+ */
+provide(Link.decl({ modName : 'pseudo', modVal : true }, /** @lends link.prototype */{
     _onPointerClick : function(e) {
         e.preventDefault();
         this.__base.apply(this, arguments);

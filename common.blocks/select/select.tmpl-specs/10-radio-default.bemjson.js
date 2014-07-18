86e191422868d9b93866db9c6ac9f443cda3b705
lapinp <lapinp@yandex-team.ru>
2014-07-18 11:26:12 +0400

    select: Write tests on templates

---
+++ common.blocks/select/select.tmpl-specs/10-radio-default.bemjson.js
@@ -0,0 +1,12 @@
+({
+    block : 'select',
+    mods : { mode : 'radio', theme : 'normal', size : 'l', focused : true },
+    name : 'select',
+    id : 1,
+    optionsMaxHeight : 100,
+    textMaxWidth : 50,
+    options : [
+        { val : 1, text : 'first' },
+        { val : 2, text : 'second', checked : true, icon : { block : 'icon', mods : { social : 'vk' } } }
+    ]
+})

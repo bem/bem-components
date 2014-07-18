3b2b37538145bf817f16e7520499ecc3fc2f58ad
lapinp <lapinp@yandex-team.ru>
2014-07-18 11:26:12 +0400

    select: Write tests on templates

---
+++ common.blocks/select/select.tmpl-specs/60-radio-check-no-checked.bemjson.js
@@ -0,0 +1,9 @@
+({
+    block : 'select',
+    mods : { mode : 'radio-check', theme : 'normal' },
+    text : 'select-radio-check',
+    options : [
+        { val : 1, text : 'first' },
+        { val : 2, text : 'second' }
+    ]
+})
---
+++ common.blocks/select/select.tmpl-specs/70-radio-check-some-checked.bemjson.js
@@ -0,0 +1,8 @@
+({
+    block : 'select',
+    mods : { mode : 'radio-check', theme : 'normal' },
+    options : [
+        { val : 1, text : 'first', checked : true },
+        { val : 2, text : 'second' }
+    ]
+})

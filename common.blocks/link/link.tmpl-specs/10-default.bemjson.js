af728d9bcef00a51e7f7f5bcee5ee55aea50f151
gela <gela-d@yandex-team.ru>
2014-08-05 18:55:50 +0400

    link: Write tests on templates

---
+++ common.blocks/link/link.tmpl-specs/10-default.bemjson.js
@@ -0,0 +1,8 @@
+({
+    block : 'link',
+    url : '//ya.ru/yandsearch?text=<script type="javascript">&amp;=test',
+    title : 'title',
+    target : '_blank',
+    tabIndex : 1,
+    content : 'link'
+})

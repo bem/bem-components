59d06b85f39c9418a4e7900ba3cf9ab622d5120b
lapinp <lapinp@yandex-team.ru>
2014-08-06 12:23:01 +0400

    add level.js, initial bemjson and png sprite

---
+++ common.blocks/rating/rating.tests/simple.bemjson.js
@@ -0,0 +1,28 @@
+({
+    block : 'rating',
+    mods : { theme : 'normal', size : 'm' },
+    name : 'rating1',
+    points : 0,
+    content : [
+        {
+            elem : 'rate',
+            text : '1'
+        },
+        {
+            elem : 'rate',
+            text : '2'
+        },
+        {
+            elem : 'rate',
+            text : '3'
+        },
+        {
+            elem : 'rate',
+            text : '4'
+        },
+        {
+            elem : 'rate',
+            text : '5'
+        }
+    ]
+});

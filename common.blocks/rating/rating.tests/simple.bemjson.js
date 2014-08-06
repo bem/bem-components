abbe8cf0e9c2a13b644eb75f94a5baba5df3aa7f
lapinp <lapinp@yandex-team.ru>
2014-08-06 14:30:35 +0400

    add styl for block and add titles for bemjson

--- common.blocks/rating/rating.tests/simple.bemjson.js
+++ common.blocks/rating/rating.tests/simple.bemjson.js
@@ -1,43 +1,54 @@
 ({
-    block : 'rating',
-    //mods : { theme : 'normal', size : 'm' },
-    //name : 'rating1',
-    //points : 0,
+    block : 'page',
+    title : 'bem-components: rating',
+    mods : { theme : 'normal' },
+    head : [
+        { elem : 'css', url : '_simple.css' }
+      //  { elem : 'js', url : '_simple.js' }
+    ],
     content : [
         {
-            elem : 'input'
-        },
-        {
-            elem : 'label',
-            text : '1'
-        },
-        {
-            elem : 'input',
-        },
-        {
-            elem : 'label',
-            text : '2'
-        },
-        {
-            elem : 'input'
-        },
-        {
-            elem : 'label',
-            text : '3'
-        },
-        {
-            elem : 'input'
-        },
-        {
-            elem : 'label',
-            text : '4'
-        },
-        {
-            elem : 'input'
-        },
-        {
-            elem : 'label',
-            text : '5'
+            block : 'rating',
+            mods : { theme : 'normal' },
+            //name : 'rating1',
+            //points : 0,
+            content : [
+                {
+                    elem : 'input'
+                },
+                {
+                    elem : 'label',
+                    text : '1'
+                },
+                {
+                    elem : 'input',
+                },
+                {
+                    elem : 'label',
+                    text : '2'
+                },
+                {
+                    elem : 'input'
+                },
+                {
+                    elem : 'label',
+                    text : '3'
+                },
+                {
+                    elem : 'input'
+                },
+                {
+                    elem : 'label',
+                    text : '4'
+                },
+                {
+                    elem : 'input'
+                },
+                {
+                    elem : 'label',
+                    text : '5'
+                }
+            ]
         }
     ]
 });

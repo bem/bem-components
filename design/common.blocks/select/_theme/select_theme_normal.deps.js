9f75ace41e4626892087180aaf3caf00abbf4775
gela <gela-d@yandex-team.ru>
2014-08-07 12:56:21 +0400

    select__tick: Increasing properties weight

--- design/common.blocks/select/_theme/select_theme_normal.deps.js
+++ design/common.blocks/select/_theme/select_theme_normal.deps.js
@@ -1,8 +1,10 @@
 ({
+    mustDeps : [
+        { block : 'button', mods : { theme : 'normal' } }
+    ],
     shouldDeps : [
         { block : 'popup', mods : { theme : 'normal' } },
         { block : 'menu', mods : { theme : 'normal' } },
-        { block : 'button', mods : { theme : 'normal' } },
         { block : 'icon' }
     ]
 })

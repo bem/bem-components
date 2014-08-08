62cac62a58c0b70ed14b0fceb8ed9041dcb4aeec
lapinp <lapinp@yandex-team.ru>
2014-08-08 11:37:33 +0400

    add i-bem.js, add rates reverse

--- common.blocks/rating/rating.tests/simple.bemjson.js
+++ common.blocks/rating/rating.tests/simple.bemjson.js
@@ -3,16 +3,16 @@
     title : 'bem-components: rating',
     mods : { theme : 'normal' },
     head : [
-        { elem : 'css', url : '_simple.css' }
-      //  { elem : 'js', url : '_simple.js' }
+        { elem : 'css', url : '_simple.css' },
+        { elem : 'js', url : '_simple.js' }
     ],
     content : [
         {
             block : 'rating',
             mods : { theme : 'normal' },
             name : 'rating',
-            points : 5.5,
-            length : '7', // строка, либо число, если убрать - сгенерирует по контенту
+            points : 3.5,
+            //length : '7', // строка, либо число, если убрать - сгенерирует по контенту
             content : [
                 {
                     elemName : 'label',

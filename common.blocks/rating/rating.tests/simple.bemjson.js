f435e0d65cf71c2fb4c90ad2da414d412a1bfdf1
lapinp <lapinp@yandex-team.ru>
2014-08-12 17:36:00 +0400

    add to bemjson blocks with size modifier

--- common.blocks/rating/rating.tests/simple.bemjson.js
+++ common.blocks/rating/rating.tests/simple.bemjson.js
@@ -6,66 +6,55 @@
         { elem : 'css', url : '_simple.css' },
         { elem : 'js', url : '_simple.js' }
     ],
-    content : [
-        { tag : 'h3', content : 'normal' },
-        {
-            block : 'rating',
-            mods : { theme : 'normal' },
-            name : 'rating',
-            points : 4,
-            //length : '17', // строка, либо число, если убрать - сгенерирует по контенту
-            content : [
-                {
-                    elemName : 'label',
-                    content : 'Ужасно 1'
-                },
-                {
-                    elemName : 'label',
-                    content : 'Плохо 2'
-                },
-                {
-                    elemName : 'label',
-                    content : 'Сойдет 3'
-                },
-                {
-                    elemName : 'label',
-                    content : 'Хорошо 4'
-                },
-                {
-                    elemName : 'label',
-                    content : 'Отлично 5'
-                }
-            ]
-        },
-        { tag : 'h3', content : 'normal disabled' },
-        {
-            block : 'rating',
-            mods : { theme : 'normal', disabled : true },
-            name : 'rating1',
-            points : 4,
-            length : '7', // строка, либо число, если убрать - сгенерирует по контенту
-            content : [
-                {
-                    elemName : 'label',
-                    content : 'Ужасно 1'
-                },
-                {
-                    elemName : 'label',
-                    content : 'Плохо 2'
-                },
-                {
-                    elemName : 'label',
-                    content : 'Сойдет 3'
-                },
-                {
-                    elemName : 'label',
-                    content : 'Хорошо 4'
-                },
-                {
-                    elemName : 'label',
-                    content : 'Отлично 5'
-                }
-            ]
-        }
-    ]
+    content : ['s', 'm'].map(function(size) {
+        return [
+            { tag : 'h3', content : 'normal ' + size },
+            {
+                block : 'rating',
+                mods : { theme : 'normal ', size : size },
+                name : 'rating' + size,
+                points : 4,
+                content : [
+                    {
+                        elemName : 'label',
+                        content : 'Ужасно 1'
+                    },
+                    {
+                        elemName : 'label',
+                        content : 'Плохо 2'
+                    },
+                    {
+                        elemName : 'label',
+                        content : 'Сойдет 3'
+                    },
+                    {
+                        elemName : 'label',
+                        content : 'Хорошо 4'
+                    },
+                    {
+                        elemName : 'label',
+                        content : 'Отлично 5'
+                    }
+                ]
+            },
+            { tag : 'h3', content : 'normal ' + size + ' length (stars)' },
+            {
+                block : 'rating',
+                mods : { theme : 'normal', size : size },
+                name : 'rating1' + size,
+                points : 4,
+                length : 10
+            },
+            { tag : 'h3', content : 'normal ' + size + ' disabled' },
+            {
+                block : 'rating',
+                mods : { theme : 'normal', size : size, disabled : true },
+                name : 'rating2' + size,
+                points : 4,
+                length : '7'
+            },
+            { tag : 'hr' }
+
+        ]
+    })
 });

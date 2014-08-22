6b35f8db6f6af968d7801ba73a5685bf5b624d69
lapinp <lapinp@yandex-team.ru>
2014-08-22 13:10:05 +0400

    rewrite api (get, set, calculate)

--- common.blocks/rating/rating.js
+++ common.blocks/rating/rating.js
@@ -1,6 +1,6 @@
 modules.define('rating', ['i-bem__dom', 'control', 'keyboard__codes'],
 
-    function(provide, BEMDOM, Control, keyCodes) {
+    function(provide, BEMDOM, Control) {
 
     provide(BEMDOM.decl({ block : this.name, baseBlock : Control }, {
         onSetMod : {
@@ -8,12 +8,7 @@ modules.define('rating', ['i-bem__dom', 'control', 'keyboard__codes'],
                 'inited' : function() {
                     this.__base.apply(this, arguments);
 
-                    this._elemLabels = this.findElem('label');
-                    this._elemLen = this._elemLabels.length + 1;
-                    this._hoveredElem = null;
-                    this._startMove = true;
-
-                    this._layerWidth = 0;
+                    this._score = 0;
                     this._on = true;
                 }
             },
@@ -21,79 +16,37 @@ modules.define('rating', ['i-bem__dom', 'control', 'keyboard__codes'],
             'focused' : {
                 'true' : function() {
                     this.__base.apply(this, arguments);
-                    this.hasMod('focused') && this.bindToDoc('keydown', this._onKeyDown);
+                    this.bindToDoc('keydown', this._onKeyDown);
                 },
 
                 '' : function() {
                     this
                         .unbindFromDoc('keydown', this._onKeyDown)
                         .__base.apply(this, arguments);
-
-                    this._removeFocusFromElem();
-                    this._resetToDefault();
                 }
             }
 
         },
 
-        setVal : function(point, total, votes) {
-            this._layerWidth = ( ( (point + total) / (votes + 1) ) * this.params.size).toFixed(1);
-            this.findElem('staticLayer').width(this._layerWidth);
+        _vote : function(point) {
+            var params = this.params;
+
+            this._score = ( ( (point + params.total) / (params.votes + 1) ) * params.size).toFixed(1);
+            this.setVal(this._score);
         },
 
-        getVal : function(elemInput) {
-            var val = Number(elemInput.context.value);
-
-            this._removeFocusFromElem();
-            this._resetToDefault();
-            this.setVal(val, 4, 1); // временно, чтобы сразу оттестить новый голос
-            this._on = false;
-            this.setMod('disabled');
-            return val;
+        setVal : function(score) {
+            this.findElem('staticLayer').width(score);
         },
 
-        _resetToDefault : function() {
-            this._elemLen = this._elemLabels.length + 1;
-            this._hoveredElem = null;
-            this._startMove = true;
-        },
-
-        _removeFocusFromElem : function() {
-            this.delMod(this._elemLabels.eq(this._hoveredElem), 'hovered');
-        },
-
-        _onKeyDown : function(e) {
-            var keyCode = e.keyCode;
-
-            this._hoveredElem !== null && (this._removeFocusFromElem());
-            this._elemLen < 0 && (this._elemLen = this._elemLabels.length - 1);
-            this._elemLen > this._elemLabels.length - 1 && (this._elemLen = 0);
-
-            if(keyCode === keyCodes.DOWN && !e.shiftKey) {
-                e.preventDefault();
-                this._elemLen++;
-            }
-
-            if(keyCode === keyCodes.UP && !e.shiftKey) {
-                !this._startMove && (this._elemLen--);
-            }
-
-            this._hoveredElem = this._elemLen - 1;
-            this.setMod(this._elemLabels.eq(this._hoveredElem), 'hovered');
-            this._startMove = false;
-
-            if(this._on && keyCode === keyCodes.SPACE) {
-                this._removeFocusFromElem();
-                this.findElem('input').eq(this._hoveredElem).click();
-                this._resetToDefault();
-                this.setMod('disabled');
-            }
+        getVal : function() {
+            return this._score;
         }
 
     }, {
         live : function() {
             this.liveBindTo('input', 'click', function(e) {
-               (this._on && this.getVal(e.currentTarget));
+               (this._on && this._vote(+e.currentTarget.context.value));
             });
 
             return this.__base.apply(this, arguments);

72964599b07bb0e1b548fe43615200bae99c05f5
DimaChervan <chervan@yandex-team.ru>
2014-08-25 13:02:29 +0400

    add default theme

--- common.blocks/rating/rating.tests/simple.bemjson.js
+++ common.blocks/rating/rating.tests/simple.bemjson.js
@@ -6,61 +6,63 @@
         { elem : 'css', url : '_simple.css' },
         { elem : 'js', url : '_simple.js' }
     ],
-    content : ['s', 'm'].map(function(size) {
-        return [
-            { tag : 'h3', content : 'normal ' + size },
-            {
-                block : 'rating',
-                mods : { theme : 'normal', size : size },
-                name : 'rating' + size,
-                total : 4,
-                votes : 1,
-                points : 4,
-                content : [
-                    {
-                        elem : 'rate',
-                        content : 'Ужасно 1'
-                    },
-                    {
-                        elem : 'rate',
-                        content : 'Плохо 2'
-                    },
-                    {
-                        elem : 'rate',
-                        content : 'Сойдет 3'
-                    },
-                    {
-                        elem : 'rate',
-                        content : 'Хорошо 4'
-                    },
-                    {
-                        elem : 'rate',
-                        content : 'Отлично 5'
-                    }
-                ]
-            },
-            { tag : 'h3', content : 'normal ' + size + ' length (stars)' },
-            {
-                block : 'rating',
-                mods : { theme : 'normal', size : size },
-                name : 'rating1' + size,
-                total : 4,
-                votes : 1,
-                points : 4,
-                granulation : 10
-            },
-            { tag : 'h3', content : 'normal ' + size + ' disabled' },
-            {
-                block : 'rating',
-                mods : { theme : 'normal', size : size, disabled : true },
-                name : 'rating2' + size,
-                total : 4,
-                votes : 1,
-                points : 4,
-                granulation : 10
-            },
-            { tag : 'hr' }
+    content : ['default', 'normal'].map(function(theme) {
+        return ['s', 'm'].map(function(size) {
+            return [
+                { tag : 'h3', content : theme + ' ' + size },
+                {
+                    block : 'rating',
+                    mods : { theme : theme, size : size },
+                    name : 'rating' + size,
+                    total : 4,
+                    votes : 1,
+                    points : 4,
+                    content : [
+                        {
+                            elem : 'rate',
+                            content : 'Ужасно 1'
+                        },
+                        {
+                            elem : 'rate',
+                            content : 'Плохо 2'
+                        },
+                        {
+                            elem : 'rate',
+                            content : 'Сойдет 3'
+                        },
+                        {
+                            elem : 'rate',
+                            content : 'Хорошо 4'
+                        },
+                        {
+                            elem : 'rate',
+                            content : 'Отлично 5'
+                        }
+                    ]
+                },
+                { tag : 'h3', content : theme + ' ' + size + ' length (stars)' },
+                {
+                    block : 'rating',
+                    mods : { theme : theme, size : size },
+                    name : 'rating1' + size,
+                    total : 4,
+                    votes : 1,
+                    points : 4,
+                    granulation : 10
+                },
+                { tag : 'h3', content : theme + ' ' + size + ' disabled' },
+                {
+                    block : 'rating',
+                    mods : { theme : theme, size : size, disabled : true },
+                    name : 'rating2' + size,
+                    total : 4,
+                    votes : 1,
+                    points : 4,
+                    granulation : 10
+                },
+                { tag : 'hr' }
 
-        ]
+            ]
+        })
     })
 });

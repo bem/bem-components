80caf31a19461195d66dd712f5d21850dbf9001e
persidskiy <persidskiy@yandex-team.ru>
2014-08-26 23:08:01 +0400

    popup: nested popup doesn't consider zIndexGroupLevel of parent popup (closes #970)

---
+++ common.blocks/popup/popup.tests/z-index-group.blocks/test-wrapper/test-wrapper.js
@@ -0,0 +1,18 @@
+modules.define('test-wrapper', ['i-bem__dom'], function(provide, BEMDOM) {
+
+provide(BEMDOM.decl(this.name, {
+    onSetMod : {
+        'js' : {
+            'inited' : function() {
+                var popup = this.findBlockInside('summon', 'popup');
+                this.findBlocksInside('summoner', 'link').forEach(function(link) {
+                    link.on('click', function(e) {
+                        popup.setTarget(e.target).setMod('visible');
+                    });
+                });
+            }
+        }
+    }
+}));
+
+});

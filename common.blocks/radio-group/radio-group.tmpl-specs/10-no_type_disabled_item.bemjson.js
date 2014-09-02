6b49594de0bb5dc3bf9b560641e98df63df4d046
Vladimir Grinenko <i@tadatuta.com>
2014-09-02 17:30:45 +0400

    Add tests for *-group mixes

--- common.blocks/radio-group/radio-group.tmpl-specs/10-no_type_disabled_item.bemjson.js
+++ common.blocks/radio-group/radio-group.tmpl-specs/10-no_type_disabled_item.bemjson.js
@@ -3,7 +3,7 @@
     mods : { theme : 'normal', size : 'm' },
     name : 'default',
     options : [
-        { val : 1, text : 'first' },
+        { val : 1, text : 'first', mix : { block : 'b1' } },
         { val : 3, text : 'third', disabled : true, checked : true }
     ]
 });

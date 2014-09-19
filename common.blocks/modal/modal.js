c3e521a2dcc4190b473edf58d7c003559f78b84b
Vladimir Varankin <nek.narqo+git@gmail.com>
2014-09-19 13:09:07 +0400

    get rid of animation modifier
    small refactoring

--- common.blocks/modal/modal.js
+++ common.blocks/modal/modal.js
@@ -22,18 +22,8 @@ provide(BEMDOM.decl(this.name, /** @lends modal.prototype */{
             }
         },
 
-        'visible' : {
-            'true' : function() {
-                // Apply the animation only at first opening, otherwise the animation will be played when page loaded.
-                this.setMod('animation');
-
-                this._popup.setMod('visible');
-
-            },
-
-            '' : function() {
-                this._popup.delMod('visible');
-            }
+        'visible' : function(modName, modVal) {
+            this._popup.setMod(modName, modVal);
         }
     }
 }, /** @lends modal */{

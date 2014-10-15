32a2ad1eb0b38efe6f33191996d202aaf5afc687
aristov <vv.aristov@gmail.com>
2014-10-15 22:28:00 +0400

    input: make _readonly modifier

---
+++ common.blocks/input/_readonly/input_readonly.js
@@ -0,0 +1,20 @@
+modules.define('input', function(provide, Input) {
+
+provide(Input.decl({ modName : 'readonly' }, {
+    beforeSetMod : {
+        readonly : {
+            '': function() {
+                this.elem('control').removeAttr('readonly');
+            }
+        }
+    },
+    onSetMod : {
+        readonly : {
+            'true': function() {
+                this.elem('control').attr('readonly', 'readonly');
+            }
+        }
+    }
+}));
+
+});

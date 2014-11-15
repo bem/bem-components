2f2a721a7a76805cd4dfb3880a41f3dc78416e51
Aleksandra Nekhaeva <lesanra@yandex-team.ru>
2014-11-15 16:53:19 +0400

    block tabs

---
+++ common.blocks/tabs/tabs.deps.js
@@ -0,0 +1,8 @@
+({
+    mustDeps : [
+        { block : 'radio-group', mods : { theme : 'islands', size : 'm', type : 'button' }, }
+    ],
+    shouldDeps : [
+        { elem : 'box', mods : { selected : true } }
+    ]
+})

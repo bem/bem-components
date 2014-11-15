39a7cdba332431bdf9281d2ad1b1dba4411208fe
gela <gela-d@yandex-team.ru>
2014-11-15 20:13:21 +0300

    corrections

---
+++ common.blocks/progressbar/progressbar.tests/simple.bemjson.js
@@ -0,0 +1,39 @@
+({
+    block : 'page',
+    title : 'progressbar',
+    mods : { theme : 'islands' },
+    head : [
+        { elem : 'css', url : '_simple.css' },
+        { elem : 'js', url : '_simple.js' }
+    ],
+    content : {
+        block : 'content',
+        attrs : { style : 'width: 400px;' },
+        content : [
+            'theme-hackaton, size-s: <br><br>',
+            {
+                block : 'progressbar',
+                mods : { theme : 'hackaton', size : 's', text : true },
+                progress : 25
+            },
+            '<br>theme-hackaton, size-m: <br><br>',
+            {
+                block : 'progressbar',
+                mods : { theme : 'hackaton', size : 'm', text : true },
+                progress : 95
+            },
+            '<br>theme-hackaton, size-m, no text (55%): <br><br>',
+            {
+                block : 'progressbar',
+                mods : { theme : 'hackaton', size : 'm', text : false },
+                progress : 55
+            },
+            '<br>theme-islands (35%): <br><br>',
+            {
+                block : 'progressbar',
+                mods : { theme : 'islands', text : false },
+                progress : 35
+            }
+        ]
+    }
+})

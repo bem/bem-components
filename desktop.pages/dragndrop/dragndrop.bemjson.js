fa4fc1e867dbac60871feea4a3958e2ad5342dac
gela <gela-d@yandex-team.ru>
2014-11-16 14:03:08 +0300

    drag-n-drop

---
+++ desktop.pages/dragndrop/dragndrop.bemjson.js
@@ -0,0 +1,40 @@
+({
+    block : 'page',
+    title : 'Drag and drop',
+    mods : { theme : 'islands' },
+    head : [
+        { elem : 'css', url : '_dragndrop.css' },
+        { elem : 'js', url : '_dragndrop.js' }
+    ],
+    content : [
+        {
+            block : 'dragndrop',
+            mods : { nose : true }
+        },
+        {
+            block : 'dragndrop',
+            attrs : { style : 'top: 80px;' },
+            mods : { eye : true }
+        },
+        {
+            block : 'dragndrop',
+            attrs : { style : 'top: 215px;' },
+            mods : { mouth : true }
+        },
+        {
+            block : 'dragndrop',
+            attrs : { style : 'top: 275px;' },
+            mods : { eye : true }
+        },
+        {
+            block : 'dragndrop',
+            attrs : { style : 'top: 375px;' },
+            mods : { hat : true }
+        },
+        {
+            block : 'dragndrop',
+            attrs : { style : 'top: 40px; right: 200px;' },
+            mods : { face : true }
+        }
+    ]
+})

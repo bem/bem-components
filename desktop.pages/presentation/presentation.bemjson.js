bd7ee76194deb0d3495682e7a0d0be4c1ea2ac9c
gela <gela-d@yandex-team.ru>
2014-11-16 16:07:03 +0300

    presentstion

--- desktop.pages/presentation/presentation.bemjson.js
+++ desktop.pages/presentation/presentation.bemjson.js
@@ -185,6 +185,35 @@
                             }
                         }
                     ]
+                },
+                {
+                    title : 'Dragndrop',
+                    content : [
+                        {
+                            block : 'dragndrop',
+                            mods : { nose : true }
+                        },
+                        {
+                            block : 'dragndrop',
+                            mods : { eye : true }
+                        },
+                        {
+                            block : 'dragndrop',
+                            mods : { mouth : true }
+                        },
+                        {
+                            block : 'dragndrop',
+                            mods : { eye : true }
+                        },
+                        {
+                            block : 'dragndrop',
+                            mods : { hat : true }
+                        },
+                        {
+                            block : 'dragndrop',
+                            mods : { face : true }
+                        }
+                    ]
                 }
             ]
         }

ce10ab8c66de3204aa0342328630c5ef8d55274b
Dmitry Manannikov <slonoed@yandex-team.ru>
2014-12-02 20:14:01 +0300

    Add select.setOptions method to dynamic update items #1100

--- common.blocks/select/select.deps.js
+++ common.blocks/select/select.deps.js
@@ -15,13 +15,21 @@
     ]
 },
 {
+    tech : 'js',
+    shouldDeps : { tech : 'bemhtml', elem : 'menu-content' }
+},
+{
     tech : 'spec.js',
-    mustDeps : { tech : 'bemhtml' }
+    mustDeps : [
+        { tech : 'bemhtml' },
+        { tech : 'bemhtml', elem : 'menu-content' }
+    ]
 },
 {
     tech : 'tmpl-spec.js',
     mustDeps : [
         { tech : 'bemhtml', block : 'select', mods : { mode : ['radio', 'check', 'radio-check'] } },
+        { tech : 'bemhtml', block : 'select', elem : 'menu-content' },
         { tech : 'bemhtml', block : 'icon' }
     ]
 }]

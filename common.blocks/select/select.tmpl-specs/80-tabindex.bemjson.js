43763b136122da19f52f52048607506063f2fcaa
Alexej Yaroshevich <alex@qfox.ru>
2014-12-10 17:41:23 +0400

    [select]: specs for tabindex

---
+++ common.blocks/select/select.tmpl-specs/80-tabindex.bemjson.js
@@ -0,0 +1,26 @@
+([
+    {
+        block : 'select',
+        mods : { mode : 'radio' },
+        tabIndex : 1,
+        options : [
+            { val : 1, text : 'first' }
+        ]
+    },
+    {
+        block : 'select',
+        mods : { mode : 'check' },
+        tabIndex : 2,
+        options : [
+            { val : 2, text : 'second' }
+        ]
+    },
+    {
+        block : 'select',
+        mods : { mode : 'radio-check' },
+        tabIndex : 3,
+        options : [
+            { val : 3, text : 'third' }
+        ]
+    }
+])

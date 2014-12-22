7cbd13ce35073be7d3a6412dc8c75663df2059d4
gela <gela-d@yandex-team.ru>
2014-12-22 11:55:49 +0300

    progressbar: Corrections after review

--- common.blocks/progressbar/progressbar.spec.js
+++ common.blocks/progressbar/progressbar.spec.js
@@ -21,21 +21,13 @@ describe('progressbar', function() {
             var value = 15;
             progressbar.setVal(value);
             progressbar.elem('bar')[0].style.width.should.be.equal(value + '%');
-            progressbar._val.should.be.equal(value);
-        });
-
-        it('should set correct percent if value is string with numbers', function() {
-            progressbar.setVal('50');
-            progressbar.elem('bar')[0].style.width.should.be.equal('50%');
-            progressbar._val.should.be.equal('50');
         });
     });
 
     describe('getVal', function() {
         it('should return actual value', function() {
             progressbar.getVal().should.be.equal(10);
-            progressbar.setVal(20);
-            progressbar.getVal().should.be.equal(20);
+            progressbar.setVal(20).getVal().should.be.equal(20);
         });
     });
 

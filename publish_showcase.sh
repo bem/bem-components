3fb658d0c34c491eccb7f8e8b530099e496126ea
Vladimir Grinenko <i@tadatuta.com>
2014-12-26 02:15:42 +0300

    publish showcase

--- publish_showcase.sh
+++ publish_showcase.sh
@@ -2,8 +2,11 @@
 
 ROOT="$(dirname $0)"
 REPORTS_ROOT="$ROOT/reports"
-SOURCE_REPORT_DIR="$ROOT/gemini-report"
+SOURCE_REPORT_DIR="$ROOT/desktop.pages/showcase"
 PUBLISH_REPORT_DIR="$ROOT/reports/$TRAVIS_BUILD_NUMBER"
+
+echo '{"freeze_paths" : {"libs/**": ":base64:","*.blocks/**": ":base64:","design/*.blocks/**": ":base64:"}}' >> .borschik
+YENV=production node_modules/.bin/magic make desktop.pages/showcase
 
 git clone -q --branch gh-pages "https://$GH_TOKEN@github.com/bem/reports.git" "$REPORTS_ROOT"
 mkdir -p "$PUBLISH_REPORT_DIR"
@@ -16,4 +19,4 @@ git add -A -f
 git commit -q -m "Report for Travis Build $TRAVIS_BUILD_NUMBER"
 git push -q origin gh-pages
 
-echo "Report is available at http://bem.github.io/reports/$TRAVIS_BUILD_NUMBER/gemini-report"
+echo "Report is available at http://bem.github.io/reports/$TRAVIS_BUILD_NUMBER/showcase/showcase.html"

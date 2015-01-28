ae7f5d684b492fc8fc1551536b42272ab2106ac6
Vladimir Grinenko <i@tadatuta.com>
2015-01-28 22:46:25 +0300

    z-index-group: add examples

---
+++ common.blocks/z-index-group/z-index-group.examples/10-z-index-group.bemjson.js
@@ -0,0 +1,29 @@
+({
+    block : 'page',
+    title : 'z-index-group',
+    head : [
+        { elem : 'css', url : '_10-z-index-group.css' }
+    ],
+    scripts : [
+        { elem : 'js', url : '_10-z-index-group.js' }
+    ],
+    mods : { theme : 'islands' },
+    content : [
+        {
+            block : 'popup',
+            js : { live : false },
+            mods : { target : 'position', theme : 'islands', size : 'bigger' },
+            mix : { block : 'z-index-group', mods : { level : 1 } },
+            content : [
+                'I am block with { level : 1 }, which is under block with { level : 2 }.'
+            ]
+        },
+        {
+            block : 'popup',
+            js : { live : false },
+            mods : { target : 'position', autoclosable : true, theme : 'islands' },
+            mix : { block : 'z-index-group', mods : { level : 2 } },
+            content : 'I am block with { level : 2 }.'
+        }
+    ]
+})

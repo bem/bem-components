3f3ee9692ac16ff76691e8cab4447c90da44a07f
Evgeniy Konstantinov <sipayrt@yandex-team.ru>
2015-07-01 15:05:35 +0300

    attach: fix BH-templates

--- common.blocks/attach/__button/attach__button.bh.js
+++ common.blocks/attach/__button/attach__button.bh.js
@@ -3,8 +3,8 @@ module.exports = function(bh) {
     bh.match('button', function(ctx) {
         if(ctx.tParam('_attach')) {
             ctx
-                .applyBase()
                 .tag('span', true)
+                .applyBase()
                 .content([
                     { block : 'attach', elem : 'control' },
                     ctx.content()

729566d66b3ef3cfee091eba10b385e7e8c64b4b
Evgeniy Konstantinov <sipayrt@yandex-team.ru>
2015-07-02 16:50:01 +0300

    progressbar: improve JAWS readability

--- common.blocks/progressbar/progressbar.bh.js
+++ common.blocks/progressbar/progressbar.bh.js
@@ -5,6 +5,10 @@ module.exports = function(bh) {
 
         ctx
             .js({ val : val })
+            .attrs({
+                role : 'progressbar',
+                'aria-valuenow' : val + '%'  // NOTE: JAWS doesn't add 'percent' automatically
+            })
             .content({
                 elem : 'bar',
                 attrs : { style : 'width:' + val + '%' }

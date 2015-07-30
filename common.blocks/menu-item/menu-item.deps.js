01445ab9b52f248287ace330c6c8086ade9cb968
Evgeniy Konstantinov <sipayrt@yandex-team.ru>
2015-07-30 16:43:52 +0300

    menu, menu-item: improve JAWS readability

--- common.blocks/menu-item/menu-item.deps.js
+++ common.blocks/menu-item/menu-item.deps.js
@@ -5,4 +5,10 @@
 {
     tech : 'spec.js',
     mustDeps : { tech : 'bemhtml', block : 'menu-item' }
+},
+{
+    tech : 'tmpl-spec.js',
+    mustDeps : [
+        { tech : 'bemhtml', block : 'link' }
+    ]
 }]

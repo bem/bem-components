69a52e04e2bf9d6d7fb0c5ec6435ed77f7c71d45
Evgeniy Konstantinov <sipayrt@yandex-team.ru>
2015-08-17 14:10:41 +0300

    Fix gemini tests for dist

--- test/dist/dist.gemini.js
+++ test/dist/dist.gemini.js
@@ -3,7 +3,7 @@ var gemini = require('gemini');
 gemini.suite('dist', function(root) {
 
     root
-        .setUrl('desktop.pages/dist/dist.html')
+        .setUrl('desktop.pages/browser-tmpl-dist-gemini/browser-tmpl-dist-gemini.html')
         .setCaptureElements('.page')
         .capture('plain');
 

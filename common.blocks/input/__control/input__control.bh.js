6c2ab478dfd79ffb3f03cb2bb400e69b5fc5ae0e
alexbaum <alexbaum@yandex-team.ru>
2015-08-24 18:48:49 +0300

    input: extend attributes support

--- common.blocks/input/__control/input__control.bh.js
+++ common.blocks/input/__control/input__control.bh.js
@@ -4,14 +4,19 @@ module.exports = function(bh) {
         ctx.tag('input');
 
         var input = ctx.tParam('_input'),
-            attrs = {
-                id : input.id,
-                name : input.name,
-                value : input.val,
-                maxlength : input.maxLength,
-                tabindex : input.tabIndex,
-                placeholder : input.placeholder
-            };
+            attrs = ctx.extend(
+                {},
+                ctx.attrs(),
+                {
+                    id : input.id,
+                    name : input.name,
+                    value : input.val,
+                    maxlength : input.maxLength,
+                    tabindex : input.tabIndex,
+                    placeholder : input.placeholder,
+                    type : input.type,
+                    required : input.required
+                });
 
         input.autocomplete === false && (attrs.autocomplete = 'off');
 

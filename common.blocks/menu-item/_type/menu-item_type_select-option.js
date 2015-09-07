0562a2b2533688dc3d23e544af50a511e235c810
aristov <vv.aristov@gmail.com>
2015-09-08 02:29:36 +0300

    jsdoc

--- common.blocks/menu-item/_type/menu-item_type_select-option.js
+++ common.blocks/menu-item/_type/menu-item_type_select-option.js
@@ -1,6 +1,15 @@
+/**
+ * @module menu-item
+ */
+
 modules.define('menu-item', function(provide, MenuItem) {
 
-provide(MenuItem.decl({ modName : 'type', modVal : 'select-option' }, {
+/**
+ * @exports
+ * @class menu-item
+ * @bem
+ */
+provide(MenuItem.decl({ modName : 'type', modVal : 'select-option' }, /** @lends menu-item.prototype */{
     onSetMod : {
         'checked' : function(_, modVal) {
             this.domElem.attr('aria-selected', !!modVal);

a33ca9ebad03212c004543bb273a7bee426f6c43
aristov <vv.aristov@gmail.com>
2015-09-09 18:54:27 +0300

    add "presentation" role where should

--- common.blocks/icon/icon.bh.js
+++ common.blocks/icon/icon.bh.js
@@ -1,6 +1,9 @@
 module.exports = function(bh) {
     bh.match('icon', function(ctx, json) {
-        var attrs = { 'aria-hidden' : 'true' },
+        var attrs = {
+                role : 'presentation',
+                'aria-hidden' : 'true' // NOTE: JAWS processes role="presentation" wrongly
+            },
             url = json.url;
         if(url) attrs.style = 'background-image:url(' + url + ')';
         ctx
--- common.blocks/radio/__text/radio__text.bh.js
+++ common.blocks/radio/__text/radio__text.bh.js
@@ -2,6 +2,9 @@ module.exports = function(bh) {
     bh.match('radio__text', function(ctx) {
         ctx
             .tag('span')
-            .attr('aria-hidden', 'true'); // NOTE: JAWS processes role="presentation" wrongly
+            .attrs({
+                role : 'presentation',
+                'aria-hidden' : 'true' // NOTE: JAWS processes role="presentation" wrongly
+            });
     });
 };

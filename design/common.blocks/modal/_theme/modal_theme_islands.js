aa4c1fce75e99cd8472aa133f5319df4880d57a7
Vyacheslav Aristov <einstain@yandex-team.ru>
2016-08-30 00:14:28 +0400

    decl => declMod (#1922)

--- design/common.blocks/modal/_theme/modal_theme_islands.js
+++ design/common.blocks/modal/_theme/modal_theme_islands.js
@@ -11,7 +11,7 @@ modules.define(
  * @class modal
  * @bem
  */
-provide(Modal.decl({ modName : 'theme', modVal : 'islands' }, /** @lends modal.prototype */{
+provide(Modal.declMod({ modName : 'theme', modVal : 'islands' }, /** @lends modal.prototype */{
     onSetMod : {
         'visible' : {
             'true' : function() {
